354cd00e681b338847bad9149186fecc2822f143
BenDuong <62047902+BenDuong@users.noreply.github.com>
2020-06-03 14:07:33 +1000

    Update Validator.java

--- src/java/uts/isd/controller/Validator.java
+++ src/java/uts/isd/controller/Validator.java
@@ -3,13 +3,14 @@ package uts.isd.controller;
 import java.io.Serializable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.servlet.http.HttpSession;
 
 public class Validator implements Serializable {
 
     private String emailPattern = "([a-zA-Z0-9]+)(([._-])([a-zA-Z0-9]+))*(@)([a-z]+)(.)([a-z]{3})((([.])[a-z]{0,2})*)";
     private String namePattern = "([A-Z][a-z]{1,20})";
     private String passwordPattern = "[a-z0-9]{6,20}";
-
+    
     public Validator() {
     }
 
@@ -23,6 +24,7 @@ public class Validator implements Serializable {
     public boolean checkEmpty(String email, String password) {
         return email.isEmpty() || password.isEmpty();
     }
+    
     
     public boolean checkEmpty(String firstName, String lastName, String email, String password) {
         return firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty();
@@ -39,4 +41,10 @@ public class Validator implements Serializable {
     public boolean validatePassword(String password) {
         return validate(passwordPattern, password);
     }
+    public void clear (HttpSession session) {
+    session.setAttribute("emailErr","Enter email");
+    session.setAttribute("passErr","Enter password");
+    session.setAttribute("existErr","");
+    session.setAttribute("nameErr","Enter name");
+}
 }
