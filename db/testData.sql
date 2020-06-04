af225a75cc63a0aef1481a02d0a51758b4acf900
Rene Cruces <rene.cruces@outlook.com>
2020-06-05 01:01:07 +1000

    Added auto increment to primary keys except ROLE table; Added test data for PRODUCT table

--- db/testData.sql
+++ db/testData.sql
@@ -7,3 +7,27 @@ INSERT INTO USERS (roleId, firstName, lastName, email, password) VALUES
     (3, 'System', 'Admin', 'admin@iotbay.com', 'admin123'),
     (1, 'John', 'Smith', 'johnsmith@gmail.com', 'apples5678'),
     (2, 'Mary', 'Adams', 'maryadams@hotmail.com', 'banana8888');
+
+
+INSERT INTO PRODUCT ("NAME", DESCRIPTION, STOCKQUANTITY, PRICE, IMAGEURL) VALUES
+    (
+        'FRDM-K64F',
+        'K24, K63, K64, mbed-Enabled Development Freedom Kinetis ARM® Cortex®-M4 MCU 32-Bit Embedded Evaluation Board',
+        1003,
+        66.39,
+        'https://i.ebayimg.com/images/g/af4AAOSwljFeHaW~/s-l1600.jpg'
+    ),
+    (
+        'Raspberry Pi 4',
+        'Raspberry Pi 4 Model B 4GB Complete Starter Pack',
+        534,
+        144.95,
+        'https://i.ebayimg.com/images/g/XGUAAOSwr9RekBmD/s-l1600.png'
+    ),
+    (
+        'TP-Link TL-WR940N',
+        'TP-Link TL-WR940N 450Mbps Wireless Router WPS WIFI Range Extender Access Point',
+        879,
+        43.50,
+        'https://i.ebayimg.com/images/g/pb0AAOSw1nVd0ef7/s-l1600.jpg'
+    );
