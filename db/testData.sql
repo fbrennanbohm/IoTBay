e5da9e2dd12b2bfdb67595e5eee495b18ce63b99
Rene Cruces <rene.cruces@outlook.com>
2020-06-05 21:11:21 +1000

    Added OrderStatus and some test data for orders

--- db/testData.sql
+++ db/testData.sql
@@ -6,7 +6,8 @@ INSERT INTO "ROLE" (ROLEID, "NAME") VALUES
 INSERT INTO USERS (roleId, firstName, lastName, email, password) VALUES
     (3, 'System', 'Admin', 'admin@iotbay.com', 'admin123'),
     (1, 'John', 'Smith', 'johnsmith@gmail.com', 'apples5678'),
-    (2, 'Mary', 'Adams', 'maryadams@hotmail.com', 'banana8888');
+    (2, 'Mary', 'Adams', 'maryadams@hotmail.com', 'banana8888'),
+    (3, 'Rene', 'Cruces', 'rene.cruces@outlook.com', 'password');
 
 
 INSERT INTO PRODUCT ("NAME", DESCRIPTION, STOCKQUANTITY, PRICE, IMAGEURL) VALUES
@@ -31,3 +32,16 @@ INSERT INTO PRODUCT ("NAME", DESCRIPTION, STOCKQUANTITY, PRICE, IMAGEURL) VALUES
         43.50,
         'https://i.ebayimg.com/images/g/pb0AAOSw1nVd0ef7/s-l1600.jpg'
     );
+
+INSERT INTO "ORDER" ("USERID", "ORDERNUMBER", "CREATEDON", "ORDERSTATUS")
+SELECT U.USERID, '00001', '2020-06-05', 'Unpaid'
+FROM USERS U
+LEFT JOIN "ORDER" O ON O.ORDERNUMBER = '00001'
+WHERE U.EMAIL = 'rene.cruces@outlook.com' AND O.ORDERID IS NULL
+
+INSERT INTO "ORDERITEM" ("ORDERID", "PRODUCTID", "QUANTITY", "PRICEPERUNIT")
+SELECT O.ORDERID, P.PRODUCTID, 2, P.PRICE
+FROM PRODUCT P
+JOIN "ORDER" O ON O.ORDERNUMBER = '00001'
+LEFT JOIN "ORDERITEM" OI ON OI.ORDERID = O.ORDERID
+WHERE P.NAME = 'FRDM-K64F' AND OI.ORDERITEMID IS NULL
